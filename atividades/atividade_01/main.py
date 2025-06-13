fd169f6845d9776d93da20b98c77ed609e90ed86
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-06-13 20:28:01 -0300

    Aula do dia 13/06/2025.

--- atividades/atividade_01/main.py
+++ atividades/atividade_01/main.py
@@ -1,3 +1,28 @@
+# laço de reptição
+while True:
+    # tratamento de exceção
+    try:
+        # entrada de dados
+        etanol = float(input("Informe o valor do etanol: R$ ").replace(",", "."))
+        gasolina = float(input("Informe o valor da gasolina: R$ ").replace(",", "."))
+        calculo = (etanol/gasolina)*100
+        result = "gasolina" if calculo > 70 else "etanol"
+
+        print(f"Resultado = {calculo:.2f}%. Compensa abastecer com {result}.")
+
+        opcao = input("Deseja refazer o cálculo? (s/n)").lower().strip()
+        match opcao:
+            case "s":
+                continue
+            case "n":
+                break
+            case _:
+                print("Opção inválida.")
+                continue
+    except Exception as e:
+        print(f"Não foi possível executar operação. {e}.")
+        continue
+
 """
 # TODO - atividade:
 # Crie um programa que receba do usuário o valor do etanol e da gasolina em reais, e informe para o usuário qual o melhor combustível para abastecer.
