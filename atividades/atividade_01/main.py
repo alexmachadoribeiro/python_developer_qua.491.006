f3582722f84d6b6ac875bc41bd355b7ec58dd73e
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-06-12 21:14:12 -0300

    Aula do dia 12/06/2025.

---
+++ atividades/atividade_01/main.py
@@ -0,0 +1,7 @@
+"""
+# TODO - atividade:
+# Crie um programa que receba do usuário o valor do etanol e da gasolina em reais, e informe para o usuário qual o melhor combustível para abastecer.
+# NOTE - o usuário poderá informar várias vezes os valores, e irá encerrar o programa quando desejar.
+# NOTE - compensa etanol caso ele tenha até 70% do valor da gasolina.
+# NOTE - DIVIRTAM-SE!!!! =D
+"""
