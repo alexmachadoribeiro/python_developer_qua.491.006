e61eff0615df25b95fddf70d9430468d76c19728
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-06-16 21:19:54 -0300

    Aula do dia 16/06/2025.

--- atividades/atividade_02/main.py
+++ atividades/atividade_02/main.py
@@ -1,3 +1,44 @@
+while True:
+    try:
+        # entrada de dados
+        nome = input("Informe seu nome: ").title().strip()
+        peso = float(input("Informe seu peso em kg: ").replace(",", "."))
+        altura = float(input("Informe sua altura em metros: ").replace(",", "."))
+        imc = peso / altura**2
+
+        print(f"O valor do IMC de {nome} é {imc:.2f}.")
+
+        if imc < 18.5:
+            print(f"{nome} está abaixo do peso.")
+        elif imc < 25:
+            print(f"{nome} está no peso ideal.")
+        elif imc < 30:
+            print(f"{nome} está acima do peso.")
+        elif imc < 35:
+            print(f"{nome} está obeso.")
+        elif imc < 40:
+            print(f"{nome} está com obesidade nível II.")
+        else:
+            print(f"{nome} está com obesidade mórbida.")
+        
+        while True:
+            prosseguir = input("Deseja refazer? (s/n) ").lower().strip()
+            if prosseguir == "s" or prosseguir == "n":
+                break
+            else:
+                print("Opção inválida.")
+                continue
+            
+        match prosseguir:
+            case "s":
+                continue
+            case "n":
+                break
+
+    except Exception as e:
+        print(f"Não foi possível calcular o IMC. {e}.")
+        continue
+
 """
 # TODO - atividade: Crie um programa que receba do usuário, o nome, o peso em kg, e a altura em metros, e calcule o valor do IMC (Índice de Massa Corporal).
 O programa deve mostrar o valor do IMC arredondado para 2 casas decimais, e mostrar o diagnóstico do usuário com base nos seguintes valores:
---
+++ atividades/atividade_03/main.py
@@ -0,0 +1,7 @@
+"""
+# TODO - atividade: Crie um programa que faça as seguintes operações:
+- Calcular área de um círculo
+- Calcular tamanho de uma circunferência
+- Sair do programa
+# NOTE - para cada loop, o programa deverá limpar o terminal
+"""
