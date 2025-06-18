5c2d26538cf1fbdc00ac9d1c927d57f0dc060fac
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-06-17 21:17:32 -0300

    Aula do dia 17/06/2025.

--- atividades/atividade_03/main.py
+++ atividades/atividade_03/main.py
@@ -1,3 +1,38 @@
+import os
+import math as m
+
+while True:
+    # menu
+    print(f"{'-'*20} MENU {'-'*20}\n")
+    print("1 - Calcular área de um círculo")
+    print("2 - Calcular tamanho de uma circunferência")
+    print("3 - Sair do programa")
+    opcao = input("Informe sua opção: ").strip()
+    os.system("cls" if os.name == "nt" else "clear")
+    try:
+        if opcao == "1" or opcao == "2":
+            raio = float(input("Informe o valor do raio: ").replace(",", "."))
+        else:
+            pass
+        os.system("cls" if os.name == "nt" else "clear")
+        match opcao:
+            case "1":
+                area = m.pi*raio**2
+                print(f"Área do círculo: {area}.")
+                continue
+            case "2":
+                circunferencia = 2*m.pi*raio
+                print(f"Tamanho da circunferência: {circunferencia}.")
+                continue
+            case "3":
+                print("Programa encerrado.")
+                break
+            case _:
+                print(f"Opção inválida.")
+                continue
+    except Exception as e:
+        print(f"Não foi possível calcular. {e}.")
+        continue
 """
 # TODO - atividade: Crie um programa que faça as seguintes operações:
 - Calcular área de um círculo
---
+++ atividades/atividade_04/main.py
@@ -0,0 +1,4 @@
+"""
+# TODO - atividade: Crie um programa que mostre a hora atual sempre sendo atualizada a cada segundo.
+# NOTE - para interromper o programa, use a tecla de atalho Ctrl+C
+"""
