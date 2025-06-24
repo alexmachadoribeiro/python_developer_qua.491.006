64f84123064c5cc7a02e07a24be1b9b0865bd9ee
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-06-23 21:16:16 -0300

    Aula do dia 23/06/2025.

---
+++ atividades/atividade_07/main.py
@@ -0,0 +1,10 @@
+"""
+# TODO - atividade: Crie um programa que faça as seguintes operações:
+- Cadastre novo nome na lista
+- Liste todos os nomes na lista
+- Pesquise por um nome na lista
+- Altere um nome na lista
+- Exclua um nome na lista
+- Sair do programa
+# NOTE - a lista deve começar vazia. Exemplo: lista = []
+"""
---
+++ projetos_console/21_lista_parte_02/main.py
@@ -0,0 +1,23 @@
+# lista
+nomes = [
+    "Alex",
+    "Joana",
+    "João",
+    "Mariana",
+    "Mário",
+    "Fernanda"
+]
+
+# exibe a lista
+for nome in nomes:
+    print(nome)
+
+# usuário informa nome a ser acrescetado na lista
+novo_nome = input("Informe o novo nome: ").title().strip()
+
+# novo nome é inserido na lista
+nomes.append(novo_nome)
+
+# re-exibe a lista
+for nome in nomes:
+    print(nome)
