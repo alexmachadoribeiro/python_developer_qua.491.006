ea2176d1246836d50bbe4f99a50efac49a44edc9
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-06-27 21:36:41 -0300

    Aula do dia 27/06/2025.

---
+++ atividades/atividade_09/main.py
@@ -0,0 +1,19 @@
+"""
+# TODO - atividade 🐍💻: Crie um programa que faça as seguintes funções:
+- Cadastre um novo usuário
+- Liste todos os usuários cadastrados
+- Altere os dados de um usuário
+- Fazer sorteio de usuário
+- Exclua um usuário
+- Saia do programa
+# NOTE - dados do usuário:
+- Nome completo
+- Data de Nascimento
+- E-mail
+- CPF
+- Telefone
+- Gênero
+- Data do cadastro (pegar do sistema)
+- Hora do cadastro (pegar do sistema)
+# NOTE - DIVIRTAM-SE!!! 💻😎👌
+"""
