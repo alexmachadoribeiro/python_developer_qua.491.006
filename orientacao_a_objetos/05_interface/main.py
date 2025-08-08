bd39d585374021b9eee7b7b6bf702eb3adbe8124
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-08-07 21:08:05 -0300

    Aula do dia 07/08/2025.

---
+++ orientacao_a_objetos/05_interface/main.py
@@ -0,0 +1,15 @@
+import classes as c
+
+def main():
+    cc = c.ContaCorrente(
+        titular="", 
+        cpf="", 
+        agencia="1010-1", 
+        conta="10101-1", 
+        saldo=0.0
+    )
+
+    # TODO continuem com a construção do programa
+
+if __name__ == "__main__":
+    main()
