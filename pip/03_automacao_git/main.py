b5b9edc4d2c34ed009d8e3fd4db2adc6a7542b1e
ead <alex.ribeiro@df.docente.senai.br>
2025-08-15 19:23:00 -0300

    Teste 03 - testando correo da automao do git

--- pip/03_automacao_git/main.py
+++ pip/03_automacao_git/main.py
@@ -16,6 +16,7 @@ def main():
     tirar_credenciais()
 
     auto.write('cd..')
+    auto.write("enter")
     auto.write(f'git config --global user.name "{usuario}"')
     auto.press("enter")
     auto.write(f'git config --global user.email "{email}"')
