8959b3084bcf9232e238bb9667d6311d10106465
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2025-09-17 21:42:46 -0300

    Aula do dia 17/09/2025.

--- 10_django/projeto_crud/crud_app/views.py
+++ 10_django/projeto_crud/crud_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Pessoa
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Olá, Mundo Django!!!")
+    pessoas = Pessoa.objects.all()
+    return render(request, "home.html", {"pessoas":pessoas})
